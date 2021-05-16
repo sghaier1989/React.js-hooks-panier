af0982d368a74dcdf81598c6f94a8ba026e4c954
Abdessalem Sghaier <sghaierabdessalem@gmail.com>
2021-05-16 22:24:08 +0200

    add lib

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './views'
+import { Provider }  from 'react-redux'
+import { store } from './src/lib/store'
 
-ReactDOM.render(<App />, document.getElementById("root")
+ReactDOM.render(
+<Provider store={store}>
+    <App  />
+</Provider>
+
+, document.getElementById("root")
 );// test
 
